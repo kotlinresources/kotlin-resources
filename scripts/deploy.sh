b342c16cf82689f4c2a4f7cee5e34f282737e48d
gyulavoros <gyula@makery.co>
2017-06-09 14:57:25 +0200

    Enable Netlify upload

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@ set -e
 
 zip -r website.zip _site
 
-#curl \
-#  -H "Content-Type: application/zip" \
-#  -H "Authorization: Bearer $NETLIFY_KEY" \
-#  --data-binary "@website.zip" \
-#  https://api.netlify.com/api/v1/sites/www.kotlinresources.com/deploys
+curl \
+  -H "Content-Type: application/zip" \
+  -H "Authorization: Bearer $NETLIFY_KEY" \
+  --data-binary "@website.zip" \
+  https://api.netlify.com/api/v1/sites/www.kotlinresources.com/deploys
 
 rm website.zip
