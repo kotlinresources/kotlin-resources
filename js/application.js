8f49d222b2becd07ed34d56a0d490ef7a3ea1998
Balazs Zsedely <zsedbal@gmail.com>
2017-08-08 16:45:06 +0200

    Improve UI

--- js/application.js
+++ js/application.js
@@ -1,33 +1,39 @@
 
 var client = algoliasearch("T9LH1UH1AV", "ffa5ba6f2753d14dc4ab0f8930a07368")
-var index = client.initIndex('kotlinresources');
+var title = client.initIndex('kotlinresources');
 var searchInput = document.getElementById("aa-search-input");
 var inputContainer = document.getElementById("aa-input-container");
 
 autocomplete('#aa-search-input', {
+  autoselect: true,
   hint: false,
-  debug: true,
-  openOnFocus: false
-}, [{
-    source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
-    displayKey: 'title',
-    templates: {
-      suggestion: function(suggestion) {
-        return suggestion._highlightResult.title.value +
-         "<div>" + suggestion._highlightResult.tags[0].value + "</div>";
+  debug: false,
+  openOnFocus: false,
+  dropdownMenu:
+    '<div class="aa-dataset-libs"></div>'
+}, [
+      {
+          source: autocomplete.sources.hits(title, {hitsPerPage: 5}),
+          displayKey: 'title',
+          templates: {
+            suggestion: function(suggestion) {
+              console.log(suggestion);
+              return "<div style='color:#0275d8!important;font-size:1.3rem'>" + suggestion._highlightResult.title.value + "</div>" +
+               "<div>" + suggestion._highlightResult.description.value + "</div>";
+            },
+            empty: '<div class="aa-empty">No matching libs</div>'
+          }
       }
-    }
-  }
-]).on('autocomplete:selected', function(event, suggestion, dataset) {
-  window.location.href = suggestion.url
-}).on('autocomplete:updated', function() {
-    if (searchInput.value.length > 0) {
-        inputContainer.classList.add("input-has-value");
-    }
-    else {
-        inputContainer.classList.remove("input-has-value");
-    }
-})
+    ]).on('autocomplete:selected', function(event, suggestion, dataset) {
+        window.location.href = suggestion.url
+      }).on('autocomplete:updated', function() {
+        if (searchInput.value.length > 0) {
+            inputContainer.classList.add("input-has-value");
+        }
+        else {
+            inputContainer.classList.remove("input-has-value");
+        }
+      })
 
 //Handle clearing the search input on close icon click
 document.getElementById("icon-close").addEventListener("click", function() {
