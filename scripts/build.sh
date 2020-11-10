20aaa91e4d690dae9d4c366eb4e53e0b51925b46
gyulavoros <gyulavoros87@gmail.com>
2020-11-10 14:37:41 +0100

    Patch build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-cd $TRAVIS_BUILD_DIR/scripts && ruby json-crawler.rb
+cd $TRAVIS_BUILD_DIR/scripts && (ruby json-crawler.rb || true)
 
 cd $TRAVIS_BUILD_DIR && bundle exec jekyll build
 
-cd $TRAVIS_BUILD_DIR/scripts && ruby html-cleaner.rb
+cd $TRAVIS_BUILD_DIR/scripts && (ruby html-cleaner.rb || true)
 
 cd $TRAVIS_BUILD_DIR && bundle exec jekyll algolia push
 
